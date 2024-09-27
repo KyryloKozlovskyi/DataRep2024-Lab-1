99e75f3de4efbfe93cb09b34016a6395df1fc9ef
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-09-27 11:13:12 +0000

    3b Completed. The addTask() function created.

--- 3.js
+++ 3.js
@@ -1,5 +1,13 @@
 // 3
 // 3.a Create an array of strings.
-let strings = ["str1", "str2", "str3", "str4", "str5"]; // Array of strings
+let Tasks = ["task1", "task2", "task3", "task4", "task5"]; // Array of strings
 
+// 3.b Create an addTask function.
+function addTask(task) { // Recieves a string (task) and adds it to the array. Returnes the length of the array.
+    Tasks.push(task); // Adds string (task) to the array
+    console.log(task + " - added to the array") // Indicates the incertion
+    return Tasks.length; // Returns length of the array
+}
 
+// addTask() call
+console.log(addTask("Cool task"))
