645831280b1b3346ccbe8e4e9e6a55b229a1a235
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-09-27 14:38:25 +0000

    3d Completed. The deleteTask() function created.

--- 3.js
+++ 3.js
@@ -22,3 +22,21 @@ function listAllTasks() { // Iterates over the array. Prints each value to the c
 
 // listAllTasks() call
 listAllTasks()
+
+// 3.d Create a deleteTask function.
+function deleteTask(task) { // Recieves string as a parameter. Removes the string form the array. Returns the length of the array.
+    let elementIndex = -1; // Stores index of the array
+    Tasks.forEach(element => { // Iterates over the array
+        elementIndex += 1
+        if (element == task) { // If value found, deletes it.
+            removedTask = Tasks.splice(elementIndex, 1) // Stores the deleted value
+            console.log(removedTask + " - deleted") // Prints deleted value to the console
+        }
+    })
+    if ((elementIndex + 1) == Tasks.length) { // If value not fount
+        console.log(task + "- not found!"); // prints "not found"
+    }
+    return Tasks.length // Returns length of the array
+}
+//deleteTask() call
+console.log(deleteTask("Cool task") + " - length of the array")
