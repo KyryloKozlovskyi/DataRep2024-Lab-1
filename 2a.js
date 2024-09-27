8e6a5ee792eb604cfad8d93f71440760de0ca2be
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-09-27 14:40:21 +0000

    2a Completed. Comments added.

--- 2a.js
+++ 2a.js
@@ -1,7 +1,7 @@
 // 2.a
 // Returns "Data Representation & Querying" string
-let returnString = ()=> {
-    console.log("Data Representation & Querying");
+let returnString = () => {
+    console.log("Data Representation & Querying"); // Output to console
 }
 
 returnString(); // Function call
